09929639e99620f7edc983656725862c2305858e
CepheidVariable <robertwoodard.usa@gmail.com>
2021-03-31 15:12:22 -0500

    add delete to main, fixed syntax errors

--- client/src/views/Main.jsx
+++ client/src/views/Main.jsx
@@ -1,5 +1,6 @@
 import { useEffect, useState } from "react";
 import axios from 'axios';
+import { Link } from "@reach/router";
 
 const Main = () =>{
     const [authors, setAuthors] = useState();
@@ -9,27 +10,35 @@ const Main = () =>{
         axios.get('http://localhost:8000/api/authors')
             .then(res => setAuthors(res.data.results))
             .catch(err => setErrors(err.response.errors));
-    });
+    }, []);
+
+    const handleDelete = id =>{
+        axios.delete('http://localhost:8000/api/authors/' + id)
+            .then(res => setAuthors(res.data.results))
+            .catch(err => setErrors(err.response.errors))
+    }
 
     return(
         <>
-            <h1>Favorite Authors</h1>
-            <button></button>
-            <table>
+            <h1 className="mt-5">Favorite Authors</h1>
+            <Link to={`/new`} className="mt-3 mb-3 btn btn-primary">Add Author</Link>
+            <table className="table col-6  ml-auto mr-auto">
                 <thead>
-                    <th>Author</th>
-                    <th>Actions</th>
+                    <tr>
+                        <th>Author</th>
+                        <th>Actions</th>
+                    </tr>
                 </thead>
                 <tbody>
-                    {authors && authors.map( (a, idx) => {
+                    {authors && authors.map( (a, idx) =>
                         <tr key={idx}>
                             <td>{a.name}</td>
                             <td>
                                 <button className="btn btn-sm btn-secondary">Edit</button>
-                                <button className="btn btn-sm btn-danger">Delete</button>
+                                <button onClick={() => handleDelete(a._id)} className="btn btn-sm btn-danger ml-2">Delete</button>
                             </td>
                         </tr>
-                    })}
+                    )}
                 </tbody>
             </table>
         </>
