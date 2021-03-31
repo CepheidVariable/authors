3df574425154d0341af7c768d2861661a20d618d
CepheidVariable <robertwoodard.usa@gmail.com>
2021-03-31 14:38:36 -0500

    app.js setup

--- client/src/App.js
+++ client/src/App.js
@@ -1,23 +1,19 @@
-import logo from './logo.svg';
 import './App.css';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import { Router } from '@reach/router';
+import Main from './views/Main';
+import Edit from './views/Edit';
+import New from './views/New';
+
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Router>
+        <Main path="/" />
+        <Edit path="/edit/:id" />
+        <New path="/new" />
+      </Router>
     </div>
   );
 }
